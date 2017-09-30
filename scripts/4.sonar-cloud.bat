975f9a941386ecdcdbed56cd404384db940df3bf
Moons <Moons@DESKTOP-OVVFGM0>
2017-09-30 09:19:23 -0500

    agrgar scripts

--- scripts/4.sonar-cloud.bat
+++ scripts/4.sonar-cloud.bat
@@ -1,5 +1,5 @@
 @echo off
-set workspace=C:\DatosJBB\WorkSpaces\Oxygen\IWVG.Forge
+set workspace=F:\WORKSPACE\Oxygen\Santiago.Perez.SVC.miw.upm.es
 ::Las variables de entorno se pueden configurar directamente en Windows cuando se tienen los permisos necesarios
 set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_65
 set M2_HOME=C:\Archivos_de_programa\apache-maven-3.3.3
@@ -14,7 +14,9 @@ echo M2_HOME   --- %M2_HOME%
 echo .
 cd %workspace%
 echo ============ mvn sonar:sonar ... =======================================================
-echo .
-call mvn sonar:sonar -Dsonar.organization=miw-upm-github -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=2c0fb8373ef9b9e73ffad70e23f1a77158fb0e37 --settings settings.xml
+echo . Se produce la cobertura 
+call - mvn org.jacoco:jacoco-maven-plugin:prepare-agent verify --settings settings.xml
+echo . Calidad de codigo sonarcloud
+call mvn sonar:sonar -Dsonar.organization=isragooprez-github -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=8e85915ebde5c3c8fcf4104e35894e9ead06e7a6 --settings settings.xml   
 pause
 
