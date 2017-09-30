4a90899e76f21e95717163d10cb476f2f76822b9
Moons <Moons@DESKTOP-OVVFGM0>
2017-09-30 10:10:03 -0500

    configs

--- scripts/4.sonar-cloud.bat
+++ scripts/4.sonar-cloud.bat
@@ -1,8 +1,8 @@
 @echo off
 set workspace=F:\WORKSPACE\Oxygen\Santiago.Perez.SVC.miw.upm.es
 ::Las variables de entorno se pueden configurar directamente en Windows cuando se tienen los permisos necesarios
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_65
-set M2_HOME=C:\Archivos_de_programa\apache-maven-3.3.3
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_131
+set M2_HOME=C:\apache-maven-3.5.0
 set PATH=%PATH%;%JAVA_HOME%\bin;%M2_HOME%\bin
 echo -----------------------------------------
 echo . (C) MIW
@@ -14,8 +14,10 @@ echo M2_HOME   --- %M2_HOME%
 echo .
 cd %workspace%
 echo ============ mvn sonar:sonar ... =======================================================
+echo ================mvn org.jacoco:jacoco-maven-plugin:prepare-agent verify --settings settings.xml  ==========
 echo . Se produce la cobertura 
-call - mvn org.jacoco:jacoco-maven-plugin:prepare-agent verify --settings settings.xml
+call mvn org.jacoco:jacoco-maven-plugin:prepare-agent verify --settings settings.xml
+
 echo . Calidad de codigo sonarcloud
 call mvn sonar:sonar -Dsonar.organization=isragooprez-github -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=8e85915ebde5c3c8fcf4104e35894e9ead06e7a6 --settings settings.xml   
 pause
